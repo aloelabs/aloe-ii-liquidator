b4b77b9fa1916d5258f7fbaa60a22bd655550257
Hayden Shively <17186559+haydenshively@users.noreply.github.com>
2023-01-25 23:20:48 -0600

    Update contracts

--- contracts/src/Liquidator.sol
+++ contracts/src/Liquidator.sol
@@ -12,53 +12,57 @@ import {IUniswapV3Factory} from "v3-core/contracts/interfaces/IUniswapV3Factory.
 
 import {ERC20, SafeTransferLib} from "solmate/utils/SafeTransferLib.sol";
 
-import "forge-std/console.sol";
-
 contract Liquidator is ILiquidator, IUniswapV3SwapCallback {
     using SafeTransferLib for ERC20;
 
     address private constant FACTORY = 0x1F98431c8aD98523631AE4a59f267346ea31F984;
 
+    receive() external payable {}
+
     function liquidate(Borrower borrower, bytes calldata data, uint256 strain) external {
         borrower.LENDER0().accrueInterest();
         borrower.LENDER1().accrueInterest();
-        borrower.liquidate(this, data, strain);
+
+        try borrower.liquidate(this, data, strain) {}
+        catch {
+            borrower.warn();
+        }
     }
 
-    function callback0(bytes calldata data, uint256 assets1, uint256 liabilities0) external {
+    function swap1For0(bytes calldata data, uint256 received1, uint256 expected0) external {
         Borrower borrower = Borrower(msg.sender);
         // Liabilities in token0, assets in token 1
         // Need to swap token1 for token0
         (, int256 amount1) = borrower.UNISWAP_POOL().swap(
             msg.sender,
             false,
-            -int256(liabilities0),
+            -int256(expected0),
             TickMath.MAX_SQRT_RATIO - 1,
             bytes("")
         );
         address rewardRecipient = abi.decode(data, (address));
         // transfer to the original caller
-        borrower.TOKEN1().transfer(rewardRecipient, assets1 - uint256(amount1));
+        borrower.TOKEN1().transfer(rewardRecipient, received1 - uint256(amount1));
     }
 
-    function callback1(bytes calldata data, uint256 assets0, uint256 liabilities1) external {
+    function swap0For1(bytes calldata data, uint256 received0, uint256 expected1) external {
         Borrower borrower = Borrower(msg.sender);
         // Liabilities in token1, assets in token 0
         // Need to swap token0 for token1
         (int256 amount0, ) = borrower.UNISWAP_POOL().swap(
             msg.sender,
             true,
-            -int256(liabilities1),
+            -int256(expected1),
             TickMath.MIN_SQRT_RATIO + 1,
             bytes("")
         );
         // transfer reward to caller
         address rewardRecipient = abi.decode(data, (address));
         // transfer to the original caller
-        borrower.TOKEN0().transfer(rewardRecipient, assets0 - uint256(amount0));
+        borrower.TOKEN0().transfer(rewardRecipient, received0 - uint256(amount0));
     }
 
-     function uniswapV3SwapCallback(int256 amount0Delta, int256 amount1Delta, bytes calldata data) public override {
+     function uniswapV3SwapCallback(int256 amount0Delta, int256 amount1Delta, bytes calldata) public override {
         address token0 = IUniswapV3Pool(msg.sender).token0();
         address token1 = IUniswapV3Pool(msg.sender).token1();
 
