e864b5924099fffc6a1adf6cd7633658486887fa
Aneesh Atri <aneesh.atri@gmail.com>
2024-04-06 15:04:03 -0400

    add config parser and other setup files
    - added a linter configuration with `.eslintrc`
    - added `.nvmrc` file to specify the node version
    - added a configuration for `jest`
    - added some tests for our parsing logic

---
+++ src/config/config.test.ts
@@ -0,0 +1,39 @@
+import { expect, test } from '@jest/globals';
+
+import { readConfig, Config } from './config';
+import invalidConfig from './test-data/invalidConfig.json';
+import validConfig from './test-data/validConfig.json';
+
+test('valid config parsed correctly', () => {
+  const parsed = readConfig(validConfig);
+  const parsedConfig: Config = {
+    chainConfigs: [
+      {
+        chainName: 'optimism',
+        chainNumber: 10,
+        factory: '0x95110C9806833d3D3C250112fac73c5A6f631E80',
+        borrowerLens: '0x8A15bfEBff7BF9ffaBBeAe49112Dc2E6C4E73Eaf',
+        borrower: '0x8A15bfEBff7BF9ffaBBeAe49112Dc2E6C4E73Eaf',
+      },
+    ],
+    multicallAddress: '0xcA11bde05977b3631167028862bE2a173976CA11',
+    createAccountTopicID: '0x1ff0a9a76572c6e0f2f781872c1e45b4bab3a0d90df274ebf884b4c11e3068f4',
+    initialDeploy: 0,
+    pollingInterval: 45000,
+    processLiquidatableInterval: 15000,
+    heartbeatInterval: 15000,
+    heartbeatTimeout: 10000,
+    clientKeepAliveTimeout: 60000,
+    sanityCheckInterval: 600000,
+    reconnectDelay: 5000,
+    reconnectMaxAttemmpts: 5,
+    errorThreshold: 5,
+    restartTimeout: 259200000,
+  };
+  expect(parsed).toEqual(parsedConfig);
+});
+
+test('invalid config returns null', () => {
+  const parsed = readConfig(invalidConfig);
+  expect(parsed).toBeNull();
+});
---
+++ src/config/config.ts
@@ -0,0 +1,139 @@
+import parse from 'parse-duration';
+import Web3 from 'web3';
+
+export interface Config {
+  chainConfigs: aloeChainConfig[];
+  multicallAddress: Address;
+  createAccountTopicID: string;
+  initialDeploy: number;
+  pollingInterval: number;
+  processLiquidatableInterval: number;
+  heartbeatInterval: number;
+  heartbeatTimeout: number;
+  clientKeepAliveTimeout: number;
+  sanityCheckInterval: number;
+  reconnectDelay: number;
+  reconnectMaxAttemmpts: number;
+  errorThreshold: number;
+  restartTimeout: number;
+}
+
+interface aloeChainConfig {
+  chainName: string;
+  chainNumber: number;
+  factory: Address;
+  borrowerLens: Address;
+  borrower: Address;
+}
+
+type Address = string;
+type ConditionFunc<T> = (input: T) => boolean;
+
+const defaultTimeUnit = 'millisecond';
+const emptyString = '';
+
+const greaterThanZero = (input: number) => {
+  return input !== undefined && input > 0;
+};
+const greaterThanOrEqualToZero = (input: number) => {
+  return input !== undefined && input >= 0;
+};
+const isAddress = (input: string, chainNumber?: number) => {
+  return input !== emptyString && Web3.utils.isAddress(input, chainNumber);
+};
+const isNotEmpty = (input: string) => {
+  return input !== emptyString;
+};
+
+export function readConfig(json: any): Config | null {
+  try {
+    const config: Config = parseConfig(json);
+    if (isValidConfig(config)) {
+      return config;
+    }
+  } catch (error) {
+    console.error('Error parsing JSON to config:', error);
+  }
+  return null;
+}
+
+function parseConfig(obj: any): Config {
+  return {
+    chainConfigs: obj.chainConfigs.map((item: aloeChainConfig) => item),
+    multicallAddress: obj.multicallAddress,
+    createAccountTopicID: obj.createAccountTopicID,
+    initialDeploy: obj.initialDeploy,
+    pollingInterval: getDuration(obj.pollingInterval),
+    processLiquidatableInterval: getDuration(obj.processLiquidatableInterval),
+    heartbeatInterval: getDuration(obj.heartbeatInterval),
+    heartbeatTimeout: getDuration(obj.heartbeatTimeout),
+    clientKeepAliveTimeout: getDuration(obj.clientKeepAliveTimeout),
+    sanityCheckInterval: getDuration(obj.sanityCheckInterval),
+    reconnectDelay: getDuration(obj.reconnectDelay),
+    reconnectMaxAttemmpts: obj.reconnectMaxAttemmpts,
+    errorThreshold: getDuration(obj.errorThreshold),
+    restartTimeout: getDuration(obj.restartTimeout),
+  } as Config;
+}
+
+function getDuration(quantity: string): number {
+  const duration = parse(quantity, defaultTimeUnit);
+  if (!duration) {
+    return 0;
+  }
+  return duration;
+}
+
+function isValidChainConfig(chainConfig: aloeChainConfig) {
+  return isAddress(chainConfig.factory) && isAddress(chainConfig.borrower) && isAddress(chainConfig.borrowerLens);
+}
+
+function isValid<T>(fieldName: string, condition: ConditionFunc<T>) {
+  return function (fieldValue: T) {
+    if (!condition(fieldValue)) {
+      console.error(`Invalid value: ${fieldValue} provided for ${fieldName}`);
+      return false;
+    }
+    return true;
+  };
+}
+
+function isValidConfig(config: Config): boolean {
+  for (const chainConfig of config.chainConfigs) {
+    if (!isValidChainConfig(chainConfig)) {
+      return false;
+    }
+  }
+
+  const stringFieldValidators = new Map([
+    [config.multicallAddress, isValid('multicallAddress', isAddress)],
+    [config.createAccountTopicID, isValid('createAccountTopicID', isNotEmpty)],
+  ]);
+
+  for (const [field, validator] of stringFieldValidators) {
+    if (!validator(field)) {
+      return false;
+    }
+  }
+
+  const numericFieldsToValidators = new Map([
+    [config.initialDeploy, isValid('initialDeploy', greaterThanOrEqualToZero)],
+    [config.pollingInterval, isValid('pollingInterval', greaterThanZero)],
+    [config.processLiquidatableInterval, isValid('processLiquidatableInterval', greaterThanZero)],
+    [config.heartbeatInterval, isValid('heartbeatInterval', greaterThanZero)],
+    [config.clientKeepAliveTimeout, isValid('clientKeepAliveTimeout', greaterThanZero)],
+    [config.sanityCheckInterval, isValid('sanityCheckInterval', greaterThanZero)],
+    [config.reconnectDelay, isValid('reconnectDelay', greaterThanZero)],
+    [config.reconnectMaxAttemmpts, isValid('reconnectMaxAttempts', greaterThanOrEqualToZero)],
+    [config.errorThreshold, isValid('errorThreshold', greaterThanZero)],
+    [config.restartTimeout, isValid('restartTimeout', greaterThanOrEqualToZero)],
+  ]);
+
+  for (const [field, validator] of numericFieldsToValidators) {
+    if (!validator(field)) {
+      return false;
+    }
+  }
+
+  return true;
+}
