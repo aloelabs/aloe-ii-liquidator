a84d4e54fc4c39260eed94aac3e53a057b6568e0
ianwoodard <17186604+IanWoodard@users.noreply.github.com>
2024-04-14 08:14:19 -0700

    Adding health check

--- src/example.ts
+++ src/example.ts
@@ -27,14 +27,14 @@ if (
   Sentry.init({
     dsn: `https://${process.env.SENTRY_DSN0}@${process.env.SENTRY_DSN1}.ingest.sentry.io/${process.env.SENTRY_DSN2}`,
     sampleRate: 0.1,
+    tracesSampleRate: 1,
     enabled:
       process.env.SENTRY_DSN0 !== undefined &&
       process.env.SENTRY_DSN1 !== undefined &&
       process.env.SENTRY_DSN2 !== undefined,
-    autoSessionTracking: false,
     release: process.env.GIT_COMMIT_SHA || undefined,
+    initialScope: { tags: { "chain_name": chain.name } },
   });
-  Sentry.setTag("chain_name", chain.name);
 }
 
 const { client, factory, borrowerLens, liquidator } = setupViemFor(
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import { fork } from "child_process";
-import { arbitrum, base, mainnet, optimism } from "viem/chains";
+import express from "express";
 import * as Sentry from "@sentry/node";
+import { arbitrum, base, mainnet, optimism } from "viem/chains";
+
+const port = process.env.PORT || 8080;
+const app = express();
 
 console.log("Starting Liquidator");
 if (
@@ -12,11 +16,11 @@ if (
   Sentry.init({
     dsn: `https://${process.env.SENTRY_DSN0}@${process.env.SENTRY_DSN1}.ingest.sentry.io/${process.env.SENTRY_DSN2}`,
     sampleRate: 0.1,
+    tracesSampleRate: 1,
     enabled:
       process.env.SENTRY_DSN0 !== undefined &&
       process.env.SENTRY_DSN1 !== undefined &&
       process.env.SENTRY_DSN2 !== undefined,
-    autoSessionTracking: false,
     release: process.env.GIT_COMMIT_SHA || undefined,
   });
 }
@@ -28,4 +32,29 @@ chains.forEach((chain) => {
   child.on("spawn", () => console.log(`Forked process for chain ${chain}`));
   child.on("error", (err) => Sentry.captureException(err, { tags: { chain } }));
   // TODO: handle errors and exits
+});
+
+app.get("/liquidator_readiness_check", (req, res) => {
+  res.send("OK");
+});
+
+const server = app.listen(port, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Server is running on port ${port}`);
+});
+
+function shutdown() {
+  server.close(() => {
+    process.exit(0);
+  });
+}
+
+process.on("SIGTERM", () => {
+  console.log("Received SIGTERM, shutting down");
+  shutdown();
+});
+
+process.on("SIGINT", () => {
+  console.log("Received SIGINT, shutting down");
+  shutdown();
 });
